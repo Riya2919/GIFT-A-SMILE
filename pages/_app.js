368886b33a36b514256928d17991f90744906f7f
AKASH SINGH <furiousakash111@gmail.com>
2022-03-29 00:24:29 +0530

    Landing page component 2 done

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <div className='bg-green-400'>
+  <Component {...pageProps}/>
+  </div>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -10,24 +10,50 @@ export default function Home() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <main>
-        <section>
-          <div className='relative'>
+        <section className='relative'>
+          <div>
             <Image
               height="600"
-              width="1280"
+              width="1440"
               src="/media/img1.jpg"
               alt="Landing page banner image"
             />
           </div>
-          <div className='absolute bottom-3 flex justify-evenly w-screen'>
-            <div className='bg-green-400 w-1/4'>
-              dfgdsf
+          <div className='absolute bottom-3 flex justify-evenly w-screen sm:bottom-5 lg:bottom-8 xl:bottom-10'>
+            <div className='bg-green-400 w-1/4 opacity-80 rounded-lg hover:scale-110 transition-all ease-in-out p-1 lg:p-3'>
+              <h1 className='text-center font-semibold sm:font-bold lg:text-xl'>420+</h1>
+              <p className='text-center text-sm sm:text-base lg:text-lg'>dolor sit amet consectetur</p>
             </div>
-            <div className='bg-green-400 w-1/4'>
-              dsgfd
+            <div className='bg-green-400 w-1/4 opacity-80 rounded-lg hover:scale-110 transition-all ease-in-out p-1 lg:p-3'>
+              <h1 className='text-center font-semibold sm:font-bold lg:text-xl'>420+</h1>
+              <p className='text-center text-sm sm:text-base lg:text-lg'>dolor sit amet consectetur</p>
             </div>
-            <div className='bg-green-400 w-1/4'>
-              dsffg
+            <div className='bg-green-400 w-1/4 opacity-80 rounded-lg hover:scale-110 transition-all ease-in-out p-1 lg:p-3'>
+              <h1 className='text-center font-semibold sm:font-bold lg:text-xl'>420+</h1>
+              <p className='text-center text-sm sm:text-base lg:text-lg'>dolor sit amet consectetur</p>
+            </div>
+          </div>
+        </section>
+        <section className='py-5'>
+          <h2 className='text-center font-semibold uppercase mb-3 sm:font-bold sm:text-lg'>What we do?</h2>
+          <div className='space-y-5 w-screen md:flex md:space-y-0 md:flex-wrap'>
+            <div className='w-3/4 sm:w-2/5 mx-auto md:p-3 lg:w-1/4 lg:p-1'>
+              <div className='bg-black opacity-80 rounded-lg p-3 shadow-xl'>
+                <h3 className='text-center font-semibold text-white mb-3 uppercase lg:text-lg'>Catering Needs</h3>
+                <p className='text-white text-center text-sm px-3 lg:text-base'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates assumenda enim, voluptatem maiores unde asperiores omnis dignissimos quia inventore, dolores officia alias quae sed reiciendis cumque voluptatum quam ratione! Accusamus?</p>
+              </div>
+            </div>
+            <div className='w-3/4 sm:w-2/5 mx-auto md:p-3 lg:w-1/4 lg:p-1'>
+              <div className='bg-black opacity-80 rounded-lg p-3 shadow-xl'>
+                <h3 className='text-center font-semibold text-white mb-3 uppercase lg:text-lg'>Finding Needs</h3>
+                <p className='text-white text-center text-sm px-3 lg:text-base'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates assumenda enim, voluptatem maiores unde asperiores omnis dignissimos quia inventore, dolores officia alias quae sed reiciendis cumque voluptatum quam ratione! Accusamus?</p>
+              </div>
+            </div>
+            <div className='w-3/4 sm:w-2/5 mx-auto md:p-3 lg:w-1/4 lg:p-1'>
+              <div className='bg-black opacity-80  rounded-lg p-3 shadow-xl'>
+                <h3 className='text-center font-semibold text-white mb-3 uppercase lg:text-lg'>Delivering smile</h3>
+                <p className='text-white text-center text-sm px-3 lg:text-base'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates assumenda enim, voluptatem maiores unde asperiores omnis dignissimos quia inventore, dolores officia alias quae sed reiciendis cumque voluptatum quam ratione! Accusamus?</p>
+              </div>
             </div>
           </div>
         </section>
