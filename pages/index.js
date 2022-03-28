e0da6c27946fcb228084b2c01e03a39616054c73
AKASH SINGH <furiousakash111@gmail.com>
2022-03-28 23:27:24 +0530

    pushing

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,33 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <>
-    
+      <Head>
+        <title>Gift-a-Smile</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <main>
+        <section>
+          <div className='relative'>
+            <Image
+              height="600"
+              width="1280"
+              src="/media/img1.jpg"
+              alt="Landing page banner image"
+            />
+          </div>
+          <div className='absolute bottom-3 flex justify-evenly w-screen'>
+            <div className='bg-green-400 w-1/4'>
+              dfgdsf
+            </div>
+            <div className='bg-green-400 w-1/4'>
+              dsgfd
+            </div>
+            <div className='bg-green-400 w-1/4'>
+              dsffg
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
